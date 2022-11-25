ca9fe395237f2302430a5aa68bd95358cdc11afb
Alex <alex.wilson1@outlook.com>
2022-11-24 19:18:58 -0600

    first iteration

---
+++ main.py
@@ -0,0 +1,100 @@
+"""Demo code showing how to estimate human head pose.
+
+There are three major steps:
+1. Detect human face in the video frame.
+2. Run facial landmark detection on the face image.
+3. Estimate the pose by solving a PnP problem.
+
+To find more details, please refer to:
+https://github.com/yinguobing/head-pose-estimation
+"""
+from argparse import ArgumentParser
+
+import cv2
+
+from mark_detector import MarkDetector
+from pose_estimator import PoseEstimator
+
+print(__doc__)
+print("OpenCV version: {}".format(cv2.__version__))
+
+
+
+if __name__ == '__main__':
+    # Before estimation started, there are some startup works to do.
+
+    # 1. Setup the video source from webcam
+
+    video_src = 1
+
+    cap = cv2.VideoCapture(video_src)
+
+    # Get the frame size. This will be used by the pose estimator.
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    # 2. Introduce a pose estimator to solve pose.
+    pose_estimator = PoseEstimator(img_size=(height, width))
+
+    # 3. Introduce a mark detector to detect landmarks.
+    mark_detector = MarkDetector()
+
+    # 4. Measure the performance with a tick meter.
+    tm = cv2.TickMeter()
+
+    # Now, let the frames flow.
+    while True:
+
+        # Read a frame.
+        frame_got, frame = cap.read()
+        if frame_got is False:
+            break
+
+        # If the frame comes from webcam, flip it so it looks like a mirror.
+        if video_src == 0:
+            frame = cv2.flip(frame, 2)
+
+        # Step 1: Get a face from current frame.
+        facebox = mark_detector.extract_cnn_facebox(frame)
+
+        # Any face found?
+        if facebox is not None:
+
+            # Step 2: Detect landmarks. Crop and feed the face area into the
+            # mark detector.
+            x1, y1, x2, y2 = facebox
+            face_img = frame[y1: y2, x1: x2]
+
+            # Run the detection.
+            tm.start()
+            marks = mark_detector.detect_marks(face_img)
+            tm.stop()
+
+            # Convert the locations from local face area to the global image.
+            marks *= (x2 - x1)
+            marks[:, 0] += x1
+            marks[:, 1] += y1
+
+            # Try pose estimation with 68 points.
+            pose = pose_estimator.solve_pose_by_68_points(marks)
+
+            # All done. The best way to show the result would be drawing the
+            # pose on the frame in realtime.
+
+            # Do you want to see the pose annotation?
+            pose_estimator.draw_annotation_box(
+                frame, pose[0], pose[1], color=(0, 255, 0))
+
+            # Do you want to see the head axes?
+            # pose_estimator.draw_axes(frame, pose[0], pose[1])
+
+            # Do you want to see the marks?
+            # mark_detector.draw_marks(frame, marks, color=(0, 255, 0))
+
+            # Do you want to see the facebox?
+            # mark_detector.draw_box(frame, [facebox])
+
+        # Show preview.
+        cv2.imshow("Preview", frame)
+        if cv2.waitKey(1) == 27:
+            break
